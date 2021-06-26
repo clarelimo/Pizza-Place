f6b2a9b711ceac3ef040f7b663f2dbe6dc1284dc
clarelimo <clare.limo@student.moringaschool.com>
2021-06-26 13:51:35 +0300

    size prices displayed for hawaiian and pepperoni

--- js/script.js
+++ js/script.js
@@ -1,32 +1,47 @@
 //pizzas
 var pizzas =[{name: "Hawaiian",
-              size:[{name:"small",price:400},{name:"medium",price:700},{name:"large",price:1200}],
+              id: "hawaiian-btn",
+              sizes:[{name:"small",price:400},{name:"medium",price:700},{name:"large",price:1200}],
               crust:[{name:"Crispy",price:50},{name:"Stuffed",price:100},{name:"Gluten-free",price:200}],
               toppings:[{name:"Mozzarella",price:100},{name:"Fetta Cheese",price:150},{name:"Caramelized Onions",price:50}]},
               
               {name: "Pepperoni",
-              size:[{name:"small",price:600},{name:"medium",price:1000},{name:"large",price:1500}],
+              id: "pepperoni-btn",
+              sizes:[{name:"small",price:600},{name:"medium",price:1000},{name:"large",price:1500}],
               crust:[{name:"Crispy",price:50},{name:"Stuffed",price:100},{name:"Gluten-free",price:200}],
               toppings:[{name:"Mozzarella",price:100},{name:"Fetta Cheese",price:150},{name:"Caramelized Onions",price:50}]},
               
               {name: "Bbq Beef",
-              size:[{name:"small",price:500},{name:"medium",price:800},{name:"large",price:1600}],
+              sizes:[{name:"small",price:500},{name:"medium",price:800},{name:"large",price:1600}],
               crust:[{name:"Crispy",price:50},{name:"Stuffed",price:100},{name:"Gluten-free",price:200}],
               toppings:[{name:"Mozzarella",price:100},{name:"Fetta Cheese",price:150},{name:"Caramelized Onions",price:50}]},
               
               {name: "Chicken Tikka",
-              size:[{name:"small",price:600},{name:"medium",price:100},{name:"large",price:1700}],
+              sizes:[{name:"small",price:600},{name:"medium",price:100},{name:"large",price:1700}],
               crust:[{name:"Crispy",price:50},{name:"Stuffed",price:100},{name:"Gluten-free",price:200}],
               toppings:[{name:"Mozzarella",price:100},{name:"Fetta Cheese",price:150},{name:"Caramelized Onions",price:50}]},
               
               {name: "Bbq Chicken",
-              size:[{name:"small",price:400},{name:"medium",price:1000},{name:"large",price:1500}],
+              sizes:[{name:"small",price:400},{name:"medium",price:1000},{name:"large",price:1500}],
               crust:[{name:"Crispy",price:50},{name:"Stuffed",price:100},{name:"Gluten-free",price:200}],
               toppings:[{name:"Mozzarella",price:100},{name:"Fetta Cheese",price:150},{name:"Caramelized Onions",price:50}]},
               
               {name: "Veggie",
-              size:[{name:"small",price:600},{name:"medium",price:1000},{name:"large",price:1600}],
+              sizes:[{name:"small",price:600},{name:"medium",price:1000},{name:"large",price:1600}],
               crust:[{name:"Crispy",price:50},{name:"Stuffed",price:100},{name:"Gluten-free",price:200}],
               toppings:[{name:"Mozzarella",price:100},{name:"Fetta Cheese",price:150},{name:"Caramelized Onions",price:50}]}  
             ];
 
+
+$(document).ready(function(){
+    pizzas.forEach(function(pizza) {
+        $("#"+pizza.id).click(function() {
+            pizza.sizes.forEach(function(size){
+                $("#"+size.name+"-"+"price").text(size.price);
+            })
+        })
+        
+    });
+
+});
+    
