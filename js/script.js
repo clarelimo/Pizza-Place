671a83b434bccb2b2f302f7ad7d55a2ad2cf856a
clarelimo <clare.limo@student.moringaschool.com>
2021-06-26 14:12:55 +0300

    size id added to other pizzas

--- js/script.js
+++ js/script.js
@@ -12,23 +12,27 @@ var pizzas =[{name: "Hawaiian",
               toppings:[{name:"Mozzarella",price:100},{name:"Fetta Cheese",price:150},{name:"Caramelized Onions",price:50}]},
               
               {name: "Bbq Beef",
+              id: "bbq-beef-btn",
               sizes:[{name:"small",price:500},{name:"medium",price:800},{name:"large",price:1600}],
               crust:[{name:"Crispy",price:50},{name:"Stuffed",price:100},{name:"Gluten-free",price:200}],
               toppings:[{name:"Mozzarella",price:100},{name:"Fetta Cheese",price:150},{name:"Caramelized Onions",price:50}]},
               
               {name: "Chicken Tikka",
+              id: "tikka-btn",
               sizes:[{name:"small",price:600},{name:"medium",price:100},{name:"large",price:1700}],
-              crust:[{name:"Crispy",price:50},{name:"Stuffed",price:100},{name:"Gluten-free",price:200}],
+              crusts:[{name:"Crispy",price:50},{name:"Stuffed",price:100},{name:"Gluten-free",price:200}],
               toppings:[{name:"Mozzarella",price:100},{name:"Fetta Cheese",price:150},{name:"Caramelized Onions",price:50}]},
               
               {name: "Bbq Chicken",
+              id: "bbq-chicken-btn",
               sizes:[{name:"small",price:400},{name:"medium",price:1000},{name:"large",price:1500}],
-              crust:[{name:"Crispy",price:50},{name:"Stuffed",price:100},{name:"Gluten-free",price:200}],
+              crusts:[{name:"Crispy",price:50},{name:"Stuffed",price:100},{name:"Gluten-free",price:200}],
               toppings:[{name:"Mozzarella",price:100},{name:"Fetta Cheese",price:150},{name:"Caramelized Onions",price:50}]},
               
               {name: "Veggie",
+              id: "veggie-btn",
               sizes:[{name:"small",price:600},{name:"medium",price:1000},{name:"large",price:1600}],
-              crust:[{name:"Crispy",price:50},{name:"Stuffed",price:100},{name:"Gluten-free",price:200}],
+              crusts:[{name:"Crispy",price:50},{name:"Stuffed",price:100},{name:"Gluten-free",price:200}],
               toppings:[{name:"Mozzarella",price:100},{name:"Fetta Cheese",price:150},{name:"Caramelized Onions",price:50}]}  
             ];
 
@@ -38,10 +42,11 @@ $(document).ready(function(){
         $("#"+pizza.id).click(function() {
             pizza.sizes.forEach(function(size){
                 $("#"+size.name+"-"+"price").text(size.price);
-            })
-        })
-        
+            });
+            pizza.crusts.forEach(function(crust){
+                $("#"+size.name+"-"+"price").text(size.price);
+            });
+        });
     });
-
 });
     
